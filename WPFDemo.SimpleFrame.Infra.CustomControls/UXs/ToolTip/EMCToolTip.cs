b37ba784abc89531198b390a53670c842e4c3afd
panming1108 <panming1108@163.com>
2019-10-14 18:04:47 +0800

    ToolTip

---
+++ WPFDemo.SimpleFrame.Infra.CustomControls/UXs/ToolTip/EMCToolTip.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WPFDemo.SimpleFrame.Infra.CustomControls.UXs.ToolTip
+{
+    public class EMCToolTip : System.Windows.Controls.ToolTip
+    {
+
+    }
+}
