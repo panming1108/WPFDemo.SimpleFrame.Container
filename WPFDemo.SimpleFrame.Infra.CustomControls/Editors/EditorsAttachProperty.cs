59d37cf8bfd928b0d2e2f710bd7b6b338fdb83c1
panming1108 <panming1108@163.com>
2019-10-18 09:55:27 +0800

    TextBox

---
+++ WPFDemo.SimpleFrame.Infra.CustomControls/Editors/EditorsAttachProperty.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFDemo.SimpleFrame.Infra.CustomControls.Editors
+{
+    public class EditorsAttachProperty
+    {
+        public static string GetWatermark(DependencyObject obj)
+        {
+            return (string)obj.GetValue(WatermarkProperty);
+        }
+
+        public static void SetWatermark(DependencyObject obj, string value)
+        {
+            obj.SetValue(WatermarkProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for Watermark.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty WatermarkProperty =
+            DependencyProperty.RegisterAttached("Watermark", typeof(string), typeof(EditorsAttachProperty), new PropertyMetadata(""));
+
+
+        public static ControlTemplate GetAttachContent(DependencyObject obj)
+        {
+            return (ControlTemplate)obj.GetValue(AttachContentProperty);
+        }
+
+        public static void SetAttachContent(DependencyObject obj, ControlTemplate value)
+        {
+            obj.SetValue(AttachContentProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for AttachContent.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AttachContentProperty =
+            DependencyProperty.RegisterAttached("AttachContent", typeof(ControlTemplate), typeof(EditorsAttachProperty), new PropertyMetadata(null));
+    }
+}
