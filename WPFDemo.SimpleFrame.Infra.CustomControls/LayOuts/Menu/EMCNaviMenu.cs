916f5ff4c641042e5a3d6634d3db60a3055a9ac8
panming1108 <panming1108@163.com>
2019-10-21 15:57:47 +0800

    菜单Menu写法

---
+++ WPFDemo.SimpleFrame.Infra.CustomControls/LayOuts/Menu/EMCNaviMenu.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace WPFDemo.SimpleFrame.Infra.CustomControls.LayOuts.Menu
+{
+    public class EMCNaviMenu : System.Windows.Controls.Menu
+    {
+        public object SelectedItem
+        {
+            get { return (object)GetValue(SelectedItemProperty); }
+            set { SetValue(SelectedItemProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SelectedItem.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.Register("SelectedItem", typeof(object), typeof(EMCNaviMenu), new PropertyMetadata(null));
+
+
+
+        public Orientation Orientation
+        {
+            get { return (Orientation)GetValue(OrientationProperty); }
+            set { SetValue(OrientationProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Orientation.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty OrientationProperty =
+            DependencyProperty.Register("Orientation", typeof(Orientation), typeof(EMCNaviMenu), new PropertyMetadata(Orientation.Horizontal));
+
+
+
+        public EMCNaviMenu()
+        {
+            MenuCommands.SelectedCommand = new DelegateCommand<object>(OnSelected);
+        }
+
+        private void OnSelected(object obj)
+        {
+            if(obj != null)
+            {
+                SelectedItem = obj;
+            }
+        }
+    }
+}
---
+++ WPFDemo.SimpleFrame.Infra.CustomControls/LayOuts/Menu/MenuCommands.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace WPFDemo.SimpleFrame.Infra.CustomControls.LayOuts.Menu
+{
+    public class MenuCommands
+    {
+        /// </summary>
+        public static ICommand SelectedCommand
+        {
+            get;
+            set;
+        }
+    }
+
+    public class DelegateCommand<T> : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (this.CanExecuteCommand != null)
+                {
+                    CommandManager.RequerySuggested += value;
+                }
+            }
+            remove
+            {
+                if (this.CanExecuteCommand != null)
+                {
+                    CommandManager.RequerySuggested -= value;
+                }
+            }
+        }
+
+        public Action<T> ExecuteCommand { get; private set; }
+        public Predicate<T> CanExecuteCommand { get; private set; }
+
+        public DelegateCommand(Action<T> executedCommand, Predicate<T> canExecutedCommand)
+        {
+            this.ExecuteCommand = executedCommand;
+            this.CanExecuteCommand = canExecutedCommand;
+        }
+        public DelegateCommand(Action<T> executedCommand) : this(executedCommand, null)
+        {
+
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return CanExecuteCommand == null || CanExecuteCommand((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            ExecuteCommand?.Invoke((T)parameter);
+        }
+    }
+}
