9cb5f9aa8d5cb68ddab6c9dcc56b70e3cca9d255
panming1108 <panming1108@163.com>
2019-11-19 17:45:49 +0800

    Json格式化

--- WPFDemo.SimpleFrame.Container/HomePage.xaml.cs
+++ WPFDemo.SimpleFrame.Container/HomePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPFDemo.SimpleFrame.Container.IViewModels;
+using WPFDemo.SimpleFrame.Infra.Ioc;
 
 namespace WPFDemo.SimpleFrame.Container
 {
@@ -22,6 +25,13 @@ namespace WPFDemo.SimpleFrame.Container
         public HomePage()
         {
             InitializeComponent();
+            DataContext = IocManagerInstance.ResolveType<IHomePageViewModel>();
+            //string text = "{\"data\":{\"name\":\"PC端对接诊断中心\",\"code\":\"pc_diag_01\",\"contact\":null,\"phone\":null,\"orgId\":3000000000002220,\"passportId\":4573859613029888,\"dispatchTarget\":1,\"id\":1000000000000126,\"remark\":\"\",\"createUser\":0,\"createUserName\":\"\",\"createTime\":\"2019-09-09T11:53:03.797\",\"updateUser\":0,\"updateUserName\":\"\",\"updateTime\":\"2019-09-09T11:53:03.797\",\"status\":0,\"rowVersion\":4573859170547201},\"code\":0,\"msg\":\"\",\"serverTime\":637093519665998191}";
+            //var jobj = JObject.Parse(text);
+
+            ////创建TreeView的数据源
+
+            //treeView.ItemsSource = jobj.Children().Select(c => JsonHeaderLogic.FromJToken(c));
         }
     }
 }
---
+++ WPFDemo.SimpleFrame.Container/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFDemo.SimpleFrame.Container.IViewModels;
+using WPFDemo.SimpleFrame.Infra.MVVM.VMOnly;
+
+namespace WPFDemo.SimpleFrame.Container.ViewModels
+{
+    public class HomePageViewModel : BaseViewModel, IHomePageViewModel
+    {
+        private IEnumerable<JsonHeaderLogic> _source;
+        public IEnumerable<JsonHeaderLogic> Source
+        {
+            get => _source;
+            set
+            {
+                _source = value;
+                OnPropertyChanged(() => Source);
+            }
+        }
+        protected async override Task Loaded()
+        {
+            string text = "{\"data\":{\"name\":\"PC端对接诊断中心\",\"code\":\"pc_diag_01\",\"contact\":null,\"phone\":null,\"orgId\":3000000000002220,\"passportId\":4573859613029888,\"dispatchTarget\":1,\"id\":1000000000000126,\"remark\":\"\",\"createUser\":0,\"createUserName\":\"\",\"createTime\":\"2019-09-09T11:53:03.797\",\"updateUser\":0,\"updateUserName\":\"\",\"updateTime\":\"2019-09-09T11:53:03.797\",\"status\":0,\"rowVersion\":4573859170547201},\"code\":0,\"msg\":\"\",\"serverTime\":637093519665998191}";
+            var jobj = JObject.Parse(text);
+
+            Source = jobj.Children().Select(c => JsonHeaderLogic.FromJToken(c));
+            await TaskEx.FromResult(0);
+        }
+
+        protected async override Task UnLoaded()
+        {
+            await TaskEx.FromResult(0);
+        }
+    }
+}
