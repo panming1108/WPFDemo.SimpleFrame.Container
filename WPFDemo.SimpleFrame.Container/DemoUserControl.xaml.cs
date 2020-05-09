d8adb1eacb97e3056edf12ae20a260392dc3d27b
panming <panming1108@163.com>
2020-05-09 10:48:27 +0800

    提交

---
+++ WPFDemo.SimpleFrame.Container/DemoUserControl.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFDemo.SimpleFrame.Container.IViewModels;
+using WPFDemo.SimpleFrame.Infra.Ioc;
+
+namespace WPFDemo.SimpleFrame.Container
+{
+    /// <summary>
+    /// DemoUserControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class DemoUserControl : UserControl
+    {
+        public DemoUserControl()
+        {
+            InitializeComponent();
+            DataContext = IocManagerInstance.ResolveType<IDemoViewModel>();
+        }
+    }
+}
---
+++ WPFDemo.SimpleFrame.Container/ViewModels/DemoViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WPFDemo.SimpleFrame.Container.IViewModels;
+using WPFDemo.SimpleFrame.Infra.MVVM.VMOnly;
+using WPFDemo.SimpleFrame.Infra.Tools;
+
+namespace WPFDemo.SimpleFrame.Container.ViewModels
+{
+    public class DemoViewModel : BaseViewModel, IDemoViewModel
+    {
+        public NotifyTaskCompletion<int> UrlByteCount { get; private set; }
+
+        public DemoViewModel()
+        {
+            UrlByteCount = new NotifyTaskCompletion<int>(MyStaticService.CountBytesInUrlAsync("https://www.Baidu.com"));
+        }
+
+        protected override async Task Loaded()
+        {
+            await TaskEx.FromResult(0);
+        }
+
+        protected override async Task UnLoaded()
+        {
+            await TaskEx.FromResult(0);
+        }
+    }
+
+    public static class MyStaticService
+    {
+        public static async Task<int> CountBytesInUrlAsync(string url)
+        {
+            // Artificial delay to show responsiveness.
+            await TaskEx.Delay(TimeSpan.FromSeconds(3)).ConfigureAwait(false);
+            // Download the actual data and count it.
+            using (var client = new HttpClient())
+            {
+                var data = await client.GetByteArrayAsync(url).ConfigureAwait(false);
+                return data.Length;
+            }
+        }
+    }
+}
