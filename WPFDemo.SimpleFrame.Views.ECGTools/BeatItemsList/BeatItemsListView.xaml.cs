eab081dd8d3eb9276620f3c29914823488b36f58
panming <panming1108@163.com>
2021-02-01 10:55:55 +0800

    心搏明细行列个数计算调整

--- WPFDemo.SimpleFrame.Views.ECGTools/BeatItemsList/BeatItemsListView.xaml.cs
+++ WPFDemo.SimpleFrame.Views.ECGTools/BeatItemsList/BeatItemsListView.xaml.cs
@@ -30,8 +30,8 @@ namespace WPFDemo.SimpleFrame.Views.ECGTools.BeatItemsList
         private readonly SelectActionFactory _selectActionFactory;
         public SelectedItemsCollection SelectedItemsCollection => _selectedItemsCollection;
         public ItemCollection Items => PART_ItemsControl.Items;
-        public int ColumnCount => Convert.ToInt32(ActualWidth / ItemWidth);
-        public int RowCount => Convert.ToInt32(ActualHeight / ItemHeight);
+        public int ColumnCount => (int)(ActualWidth / ItemWidth);
+        public int RowCount => (int)(ActualHeight / ItemHeight);
         public int CurrentMoveIndex { get; set; }
 
         public double ItemHeight
