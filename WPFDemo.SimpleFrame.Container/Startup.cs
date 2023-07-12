cb4b2782961683a16378c2b3a37878f5947dbd94
panming <panming1108@163.com>
2023-07-12 15:21:17 +0800

    全局异常捕获

--- WPFDemo.SimpleFrame.Container/Startup.cs
+++ WPFDemo.SimpleFrame.Container/Startup.cs
@@ -4,12 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Windows;
 using WPFDemo.SimpleFrame.Infra.Application;
+using WPFDemo.SimpleFrame.Infra.Helper;
+using WPFDemo.SimpleFrame.Infra.Ioc;
 using WPFDemo.SimpleFrame.Infra.Models;
 
 namespace WPFDemo.SimpleFrame.Container
 {
     public class Startup : AppBase
     {
+        public Startup()
+        {
+            DispatcherUnhandledException += Startup_DispatcherUnhandledException;
+        }
+
+        private void Startup_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            var str = "系统异常\r\n" + e.Exception.Message + "\r\n" + e.Exception.StackTrace + "\r\n" + e.Exception.InnerException;
+            LogHelper.Error(str);
+            MessageBox.Show(str);
+        }
+
         protected override void App_Startup(object sender, StartupEventArgs e)
         {
             string[] lists = new string[]
